ae2f6832bd6d111600fd9f2583750d93d5585f0f
Michael Millar <mcmelord@gmail.com>
2022-01-20 23:01:08 -0700

    customized product view page, add image support

--- angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -9,7 +9,7 @@ import { ProductService } from 'src/app/services/product.service';
 })
 export class ProductListComponent implements OnInit {
 
-  products: Product[];
+  products!: Product[];
   constructor(private productService: ProductService) { }
 
   ngOnInit(): void {
