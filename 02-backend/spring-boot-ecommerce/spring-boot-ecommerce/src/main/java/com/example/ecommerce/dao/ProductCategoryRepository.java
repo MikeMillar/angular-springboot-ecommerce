c1c6bf5a0847648fdb5c226434b18d7f307ed5ec
Michael Millar <mcmelord@gmail.com>
2022-01-20 22:06:50 -0700

    add cross origin support for development

--- 02-backend/spring-boot-ecommerce/spring-boot-ecommerce/src/main/java/com/example/ecommerce/dao/ProductCategoryRepository.java
+++ 02-backend/spring-boot-ecommerce/spring-boot-ecommerce/src/main/java/com/example/ecommerce/dao/ProductCategoryRepository.java
@@ -3,6 +3,7 @@ package com.example.ecommerce.dao;
 import com.example.ecommerce.entity.ProductCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 /**
  * created by:
@@ -10,6 +11,7 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
  * date: 1/20/2022
  */
 
+@CrossOrigin("http://localhost:4200")
 @RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
 
--- 02-backend/spring-boot-ecommerce/spring-boot-ecommerce/src/main/java/com/example/ecommerce/dao/ProductRepository.java
+++ 02-backend/spring-boot-ecommerce/spring-boot-ecommerce/src/main/java/com/example/ecommerce/dao/ProductRepository.java
@@ -2,12 +2,15 @@ package com.example.ecommerce.dao;
 
 import com.example.ecommerce.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 /**
  * created by:
  * author: MichaelMillar
  * date: 1/20/2022
  */
+
+@CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
 }
