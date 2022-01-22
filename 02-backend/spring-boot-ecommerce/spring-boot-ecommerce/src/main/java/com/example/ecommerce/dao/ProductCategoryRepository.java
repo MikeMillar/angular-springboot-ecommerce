1cebef2a2eede5a1928292201e2a97e7a3aee925
Michael Millar <mcmelord@gmail.com>
2022-01-22 10:48:36 -0700

    commit missed springboot changes

--- 02-backend/spring-boot-ecommerce/spring-boot-ecommerce/src/main/java/com/example/ecommerce/dao/ProductCategoryRepository.java
+++ 02-backend/spring-boot-ecommerce/spring-boot-ecommerce/src/main/java/com/example/ecommerce/dao/ProductCategoryRepository.java
@@ -1,9 +1,13 @@
 package com.example.ecommerce.dao;
 
+import com.example.ecommerce.entity.Product;
 import com.example.ecommerce.entity.ProductCategory;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * created by:
@@ -14,5 +18,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 @CrossOrigin("http://localhost:4200")
 @RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
+
+
 
 }
