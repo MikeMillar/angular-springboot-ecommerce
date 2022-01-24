2485384ef412e5e4d18285de44c9167cc0b1b230
Michael Millar <mcmelord@gmail.com>
2022-01-23 18:06:22 -0700

    add entities for state and country in backend, create repositories, update config for read-only, dynamic credit card month/year support added

---
+++ 02-backend/spring-boot-ecommerce/spring-boot-ecommerce/src/main/java/com/example/ecommerce/dao/CountryRepository.java
@@ -0,0 +1,8 @@
+package com.example.ecommerce.dao;
+/** 
+  * created by:
+  *  author: MichaelMillar
+  *  date: 1/23/2022
+  */
+public interface CountryRepository {
+}
---
+++ 02-backend/spring-boot-ecommerce/spring-boot-ecommerce/src/main/java/com/example/ecommerce/entity/Country.java
@@ -0,0 +1,8 @@
+package com.example.ecommerce.entity;
+/** 
+  * created by:
+  *  author: MichaelMillar
+  *  date: 1/23/2022
+  */
+public class Country {
+}
